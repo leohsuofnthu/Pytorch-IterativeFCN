b13dac85cd63382e875b8c67e6130be27d5554dd
leohsuofnthu <leohsuofnthu@gmail.com>
2019-09-18 15:26:29 -0400

    extract patch function done

--- dataset.py
+++ dataset.py
@@ -62,49 +62,120 @@ class CSI_Dataset(Dataset):
         #depend on the original data (CSI data value is from 0 ~ 4095)
         img = img * self.linear_att - self.offset
         
-        #list available vertebrae
-        verts = np.unique(mask)
-        print(verts)
-        chosen_vert = verts[randint(1, len(verts)-1)]
-        print(chosen_vert)
+        img_patch, ins_patch, gt_patch, c_label = extract_random_patch(img, 
+                                                              mask)
         
-        #create corresponde instance memory and ground truth
-        ins_memory = np.copy(mask)
-        ins_memory[ins_memory >= chosen_vert] = 0
-        ins_memory[ins_memory > 0] = 1
-        print(np.unique(ins_memory))
+        return img_patch, ins_patch, gt_patch, c_label
         
-        gt = np.copy(mask)
-        gt[gt != chosen_vert] = 0
-        gt[gt > 0] = 1
-        print(np.unique(gt))
+    
+#%% Extract the 128*128*128 patch
+def extract_random_patch(img, mask, patch_size=128):
+    
+    #list available vertebrae
+    verts = np.unique(mask)
+    print('mask values:', verts)
+    chosen_vert = verts[randint(1, len(verts)-1)]
+    print('chosen_vert:', chosen_vert)
+    
+    #create corresponde instance memory and ground truth
+    ins_memory = np.copy(mask)
+    ins_memory[ins_memory >= chosen_vert] = 0
+    ins_memory[ins_memory > 0] = 1
+#    print(np.unique(ins_memory))
+    
+    gt = np.copy(mask)
+    gt[gt != chosen_vert] = 0
+    gt[gt > 0] = 1
+#    print(np.unique(gt))
 
+    
+    if np.random.rand() <= 0.25:
+        print('Random non-vert Patch')
+        patch_center = [np.random.randint(0, s) for s in img.shape]
+        lower = [0, 0, 0]
+        upper = [img.shape[0], img.shape[1], img.shape[2]]
+        x = patch_center[0]
+        y = patch_center[1]
+        z = patch_center[2]
+        
+    else:
+        print('Random vert Patch')
         indices = np.nonzero(mask == chosen_vert)
-        print(indices[1])
         lower = [np.min(i) for i in indices]
         upper = [np.max(i) for i in indices]
-        
-        print(lower)
-        print(upper)
-        
         #random center of patch
         x = randint(lower[0], upper[0])
         y = randint(lower[1], upper[1])
         z = randint(lower[2], upper[2])
-        
-        print(x,y,z)
-        print(ins_memory[indices[0][0],indices[1][0],indices[2][0]])
-        
-        #extract the patch and padding
-        patch_size = 128
-        img_patch = img[x-patch_size/2:x+patch_size/2, y-patch_size/2:y+patch_size/2, z-patch_size/2:z+patch_size/2]
-        
-        return img, mask
     
-#%% Extract the 128*128*128 patch
-#def extract_patch(img, mask):
+    #extract the patch and padding
+    x_low = int(max(x-patch_size/2, lower[0]))
+    x_up = int(min(x+patch_size/2, upper[0]))
+    
+    y_low = int(max(y-patch_size/2, lower[1]))
+    y_up = int(min(y+patch_size/2, upper[1]))
+    
+    z_low = int(max(z-patch_size/2, lower[2]))
+    z_up = int(min(z+patch_size/2, upper[2]))
+    
+    x_pad, y_pad, z_pad = np.zeros(2), np.zeros(2), np.zeros(2)
+    
+    img_patch = img[x_low:x_up, y_low:y_up,z_low:z_up]
+    ins_patch = ins_memory[x_low:x_up, y_low:y_up,z_low:z_up]
+    gt_patch = gt[x_low:x_up, y_low:y_up,z_low:z_up]
     
     
+    sitk.WriteImage(sitk.GetImageFromArray(img_patch), './img.nrrd', True)
+    sitk.WriteImage(sitk.GetImageFromArray(ins_patch), './ins.nrrd', True)
+    sitk.WriteImage(sitk.GetImageFromArray(gt_patch), './gt.nrrd', True)
+    
+    
+    #give the label of completeness(partial or complete)
+    vol = np.count_nonzero(gt == 1)
+    print(vol)
+    sample_vol = np.count_nonzero(gt_patch == 1 )
+    print(sample_vol)
+    
+    print('vol:{:.6f}'.format(float(sample_vol/(vol+0.0001))))
+    
+    c_label = 0 if float(sample_vol/(vol+0.0001)) < 0.98 else 1
+
+    #paddding the patch to 128*128*128
+    if (patch_size - img_patch.shape[0])%2 == 1:
+        x_pad[0] = int((patch_size - img_patch.shape[0])/2) 
+        x_pad[1] = x_pad[0]+1
+    else:
+        x_pad[0] = (patch_size - img_patch.shape[0])/2
+        x_pad[1] = x_pad[0]
+        
+    if (patch_size - img_patch.shape[1])%2 == 1:
+        y_pad[0] = int((patch_size - img_patch.shape[1])/2) 
+        y_pad[1] = y_pad[0]+1
+    else:
+        y_pad[0] = (patch_size - img_patch.shape[1])/2
+        y_pad[1] = y_pad[0]    
+        
+    if (patch_size - img_patch.shape[2])%2 == 1:
+        z_pad[0] = int((patch_size - img_patch.shape[2])/2) 
+        z_pad[1] = z_pad[0]+1
+    else:
+        z_pad[0] = (patch_size - img_patch.shape[2])/2
+        z_pad[1] = z_pad[0]
+    
+    x_pad = x_pad.astype(int)
+    y_pad = y_pad.astype(int)
+    z_pad = z_pad.astype(int)
+    
+    img_patch = np.pad(img_patch, ((x_pad[0], x_pad[1]), (y_pad[0], y_pad[1]), (z_pad[0], z_pad[1])), 'constant', constant_values= img.min())
+    ins_patch = np.pad(ins_patch, ((x_pad[0], x_pad[1]), (y_pad[0], y_pad[1]), (z_pad[0], z_pad[1])), 'constant', constant_values=ins_memory.min())
+    gt_patch = np.pad(gt_patch, ((x_pad[0], x_pad[1]), (y_pad[0], y_pad[1]), (z_pad[0], z_pad[1])), 'constant', constant_values=mask.min())
+    
+    img_patch = np.expand_dims(img_patch, axis=0)
+    ins_patch = np.expand_dims(ins_patch, axis=0)
+    gt_patch = np.expand_dims(gt_patch, axis=0)
+    c_label = np.expand_dims(c_label, axis=0)
+    
+    return img_patch, ins_patch, gt_patch, c_label
 
 
 #%%%
@@ -112,7 +183,13 @@ train_dataset = CSI_Dataset('D:/Project III- Iterative Fully Connected Network f
 
 dataloader_train = DataLoader(train_dataset, batch_size=1, shuffle=True)
 
-i, m = next(iter(dataloader_train))
+img_patch, ins_patch, gt_patch, c_label = next(iter(dataloader_train))
+
+print(c_label.item())
+
+
+
+
 
 
 
