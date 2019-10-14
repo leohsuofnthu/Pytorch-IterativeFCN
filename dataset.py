20feb26da2886d7396da772e2867ec03b4ac655c
leohsuofnthu <leohsuofnthu@gmail.com>
2019-10-14 15:18:05 -0400

    ready for new data aug

--- dataset.py
+++ dataset.py
@@ -222,20 +222,20 @@ def extract_random_patch(img, mask, weight, i, patch_size=128):
     
     #Randomly Data Augmentation
     # 50% chance elastic deformation
-    if 0:
+    if np.random.rand() > 0.5:
         print('elastic deform')
         img_patch, gt_patch, ins_patch, weight_patch = elastic_transform(img_patch, gt_patch, ins_patch, weight_patch, alpha=20, sigma=5)
     # 50% chance gaussian blur
-    if 0:
+    if np.random.rand() > 0.5:
         print('gaussian blur')
         img_patch = gaussian_blur(img_patch)
     # 50% chance gaussian noise
-    if 0:
+    if np.random.rand() > 0.5:
         print('gaussian noise')
         img_patch = gaussian_noise(img_patch)
     
-    if 1:
-        print('crop')
+    if np.random.rand() > 0.5:
+        print('Random crop along z-axis')
         img_patch, ins_patch, gt_patch, weight_patch = random_crop(img_patch, ins_patch, gt_patch
         ,weight_patch)
     
@@ -246,8 +246,7 @@ def extract_random_patch(img, mask, weight, i, patch_size=128):
     
     #print('visible volume:{:.6f}'.format(float(sample_vol/(vol+0.0001))))
     c_label = 0 if float(sample_vol/(vol+0.0001)) < 0.98 else 1
-    
-    print(c_label)
+
 
     
     img_patch = np.expand_dims(img_patch, axis=0)
@@ -261,29 +260,29 @@ def extract_random_patch(img, mask, weight, i, patch_size=128):
 
 
 #%% Test purpose
-
-train_dataset = CSI_Dataset('D:/Project III- Iterative Fully Connected Network for Vertebrae Segmentation/Pytorch-IterativeFCN/crop_isotropic_dataset')
-
-dataloader_train = DataLoader(train_dataset, batch_size=1, shuffle=True)
-
-img_patch, ins_patch, gt_patch, weight, c_label = next(iter(dataloader_train))
-
-print(img_patch.shape)
-print(gt_patch.shape)
-
-
-img_patch = torch.squeeze(img_patch)
-ins_patch = torch.squeeze(ins_patch)
-gt_patch = torch.squeeze(gt_patch)
-weight = torch.squeeze(weight)
-
-
-#produce 17000 training samples, and 3000 test sample
-
-sitk.WriteImage(sitk.GetImageFromArray(img_patch.numpy()), './img.nrrd', True)
-sitk.WriteImage(sitk.GetImageFromArray(gt_patch.numpy()), 'gt.nrrd', True)
-sitk.WriteImage(sitk.GetImageFromArray(ins_patch.numpy()), 'ins.nrrd', True)
-sitk.WriteImage(sitk.GetImageFromArray(weight.numpy()), 'wei.nrrd', True)
+#
+#train_dataset = CSI_Dataset('D:/Project III- Iterative Fully Connected Network for Vertebrae Segmentation/Pytorch-IterativeFCN/crop_isotropic_dataset')
+#
+#dataloader_train = DataLoader(train_dataset, batch_size=1, shuffle=True)
+#
+#img_patch, ins_patch, gt_patch, weight, c_label = next(iter(dataloader_train))
+#
+#print(img_patch.shape)
+#print(gt_patch.shape)
+#
+#
+#img_patch = torch.squeeze(img_patch)
+#ins_patch = torch.squeeze(ins_patch)
+#gt_patch = torch.squeeze(gt_patch)
+#weight = torch.squeeze(weight)
+#
+#
+##produce 17000 training samples, and 3000 test sample
+#
+#sitk.WriteImage(sitk.GetImageFromArray(img_patch.numpy()), './img.nrrd', True)
+#sitk.WriteImage(sitk.GetImageFromArray(gt_patch.numpy()), 'gt.nrrd', True)
+#sitk.WriteImage(sitk.GetImageFromArray(ins_patch.numpy()), 'ins.nrrd', True)
+#sitk.WriteImage(sitk.GetImageFromArray(weight.numpy()), 'wei.nrrd', True)
 
 
 
