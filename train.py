11caec0b3409b3a109f7455c458b5eb1e324b9e7
leohsuofnthu <leohsuofnthu@gmail.com>
2020-02-13 15:09:39 -0500

    add support for resume training

--- train.py
+++ train.py
@@ -55,7 +55,7 @@ def train_single(model, device, img_patch, ins_patch, gt_patch, weight, c_label,
     c_loss = F.binary_cross_entropy(torch.unsqueeze(C, dim=0), c_label)
     train_loss = s_loss + c_loss
 
-    logging.info("train_dice_coef: %s, S Loss: %s, C Loss: %s" %(train_dice_coef, s_loss.item(), c_loss.item()))
+    logging.info("train_dice_coef: %s, S Loss: %s, C Loss: %s" % (train_dice_coef, s_loss.item(), c_loss.item()))
 
     if C.round() == c_label:
         correct = 1
@@ -67,7 +67,7 @@ def train_single(model, device, img_patch, ins_patch, gt_patch, weight, c_label,
     return train_loss.item(), correct, train_dice_coef
 
 
-def test_single(device, img_patch, ins_patch, gt_patch, weight, c_label):
+def test_single(model, device, img_patch, ins_patch, gt_patch, weight, c_label):
     torch.cuda.empty_cache()
 
     model.eval()
@@ -96,7 +96,7 @@ def test_single(device, img_patch, ins_patch, gt_patch, weight, c_label):
     s_loss = lamda * FP + FN
     c_loss = F.binary_cross_entropy(torch.unsqueeze(C, dim=0), c_label)
 
-    logging.info("train_dice_coef: %s, S Loss: %s, C Loss: %s" %(test_dice_coef, s_loss.item(), c_loss.item()))
+    logging.info("train_dice_coef: %s, S Loss: %s, C Loss: %s" % (test_dice_coef, s_loss.item(), c_loss.item()))
 
     if C.round() == c_label:
         correct = 1
@@ -116,20 +116,20 @@ if __name__ == "__main__":
                         help='path of processed dataset')
     parser.add_argument('--weight', type=str, default='./weights',
                         help='path of processed dataset')
-    parser.add_argument('--checkpoints', type=str, default='./weights',
+    parser.add_argument('--checkpoints', type=str, default='./checkpoints',
                         help='path of training snapshot')
-    parser.add_argument('--resume', type=bool, default=False,
+    parser.add_argument('--resume', type=bool, default=True,
                         help='resume training by loading last snapshot')
     parser.add_argument('--batch-size', type=int, default=1, metavar='N',
                         help='input batch size for training (default: 64)')
     parser.add_argument('--test-batch-size', type=int, default=1, metavar='N',
-                        help='input batch size for testing (default: 1000)')
-    parser.add_argument('--iterations', type=int, default=100000, metavar='N',
-                        help='number of iterations to train (default: 5000)')
-    parser.add_argument('--log_interval', type=int, default=1000, metavar='N',
-                        help='number of iterations to train (default: 5000)')
-    parser.add_argument('--eval_iters', type=int, default=20, metavar='N',
-                        help='number of iterations to train (default: 5000)')
+                        help='input batch size for testing (default: 1)')
+    parser.add_argument('--iterations', type=int, default=20, metavar='N',
+                        help='number of iterations to train (default: 80000)')
+    parser.add_argument('--log_interval', type=int, default=2, metavar='N',
+                        help='number of iterations to log (default: 1000)')
+    parser.add_argument('--eval_iters', type=int, default=1, metavar='N',
+                        help='number of iterations to train (default: 20)')
     parser.add_argument('--lr', type=float, default=1e-3, metavar='LR',
                         help='learning rate (default: 0.01)')
     parser.add_argument('--seed', type=int, default=1, metavar='S',
@@ -169,23 +169,21 @@ if __name__ == "__main__":
     train_interval = args.log_interval
     eval_interval = args.eval_iters
 
-    epochs = 0
+    iteration = 1
 
     if args.resume:
         logging.info("Resume Training: Load states from latest checkpoint.")
-        checkpoint = torch.load(os.path.join(args.checkpoints, 'latest_checkpoints'))
+        checkpoint = torch.load(os.path.join(args.checkpoints, 'latest_checkpoints.pth'))
         model.load_state_dict(checkpoint['model_state_dict'])
         optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-        epochs = checkpoint['epoch']
+        iteration = checkpoint['iteration']
         train_loss = checkpoint['train_loss']
         test_loss = checkpoint['test_loss']
         train_acc = checkpoint['train_acc']
         test_acc = checkpoint['test_acc']
 
     # Start Training
-    for epoch in range(int(total_iteration / train_interval) - epochs):
-
-        epoch += epochs
+    while iteration < args.iterations+1:
         start_time = time.time()
         epoch_train_dice = []
         epoch_test_dice = []
@@ -196,69 +194,73 @@ if __name__ == "__main__":
         correct_train_count = 0
         correct_test_count = 0
 
-        # training process
-        for i in range(train_interval):
-            img_patch, ins_patch, gt_patch, weight, c_label = next(iter(train_loader))
-            t_loss, t_c, t_dice = train_single(model, device, img_patch, ins_patch, gt_patch, weight, c_label,
-                                               optimizer)
-            epoch_train_loss.append(t_loss)
-            epoch_train_dice.append(t_dice)
-            correct_train_count += t_c
+        img_patch, ins_patch, gt_patch, weight, c_label = next(iter(train_loader))
+        t_loss, t_c, t_dice = train_single(model, device, img_patch, ins_patch, gt_patch, weight, c_label, optimizer)
+        epoch_train_loss.append(t_loss)
+        epoch_train_dice.append(t_dice)
+        correct_train_count += t_c
 
-        epoch_train_accuracy = correct_train_count / train_interval
-        avg_train_loss = sum(epoch_train_loss) / len(epoch_train_loss)
-        avg_train_dice = sum(epoch_train_dice) / len(epoch_train_dice)
+        if iteration > 1 and iteration % args.log_interval:
+            avg_train_loss = sum(epoch_train_loss) / len(epoch_train_loss)
+            avg_train_dice = (sum(epoch_train_dice) / len(epoch_train_dice)) * 100
+            epoch_train_accuracy = (correct_train_count / train_interval) * 100
 
-        logging.info('Train Epoch: {} \t Loss: {:.6f}\t acc: {:.6f}%\t dice: {:.6f}%'.format(epoch,
-                                                                                             avg_train_loss,
-                                                                                             epoch_train_accuracy * 100,
-                                                                                             avg_train_dice * 100))
+            logging.info('Iter {}-{}: \t Loss: {:.6f}\t acc: {:.6f}%\t dice: {:.6f}%'.format(
+                                                                                           iteration-args.log_interval,
+                                                                                           iteration,
+                                                                                           avg_train_loss,
+                                                                                           epoch_train_accuracy,
+                                                                                           avg_train_dice))
 
-        if avg_train_loss < best_train_loss:
-            best_train_loss = avg_train_loss
-            logging.info('--- Saving model at Avg Train Dice:{:.2f}%  ---'.format(avg_train_dice * 100))
-            torch.save(model.state_dict(), os.path.join(args.weight, '.IterativeFCN_best_train.pth'))
+            if avg_train_loss < best_train_loss:
+                best_train_loss = avg_train_loss
+                logging.info('--- Saving model at Avg Train Dice:{:.2f}%  ---'.format(avg_train_dice))
+                torch.save(model.state_dict(), os.path.join(args.weight, '.IterativeFCN_best_train.pth'))
 
-        # validation process
-        for i in range(eval_interval):
-            img_patch, ins_patch, gt_patch, weight, c_label = next(iter(test_loader))
-            v_loss, v_c, v_dice = test_single(model, device, img_patch, ins_patch, gt_patch, weight, c_label)
-            epoch_test_loss.append(v_loss)
-            epoch_test_dice.append(v_dice)
-            correct_test_count += v_c
+            # validation process
+            for i in range(args.eval_iters):
+                img_patch, ins_patch, gt_patch, weight, c_label = next(iter(test_loader))
+                v_loss, v_c, v_dice = test_single(model, device, img_patch, ins_patch, gt_patch, weight, c_label)
+                epoch_test_loss.append(v_loss)
+                epoch_test_dice.append(v_dice)
+                correct_test_count += v_c
 
-        epoch_test_accuracy = correct_test_count / eval_interval
-        avg_test_loss = sum(epoch_test_loss) / len(epoch_test_loss)
-        avg_test_dice = sum(epoch_test_dice) / len(epoch_test_dice)
+            avg_test_loss = sum(epoch_test_loss) / len(epoch_test_loss)
+            avg_test_dice = (sum(epoch_test_dice) / len(epoch_test_dice)) * 100
+            epoch_test_accuracy = (correct_test_count / eval_interval) * 100
 
-        logging.info('Validation Epoch: {} \t Loss: {:.6f}\t acc: {:.6f}%\t dice: {:.6f}%'.format(epoch,
-                                                                                                  avg_test_loss,
-                                                                                                  epoch_test_accuracy * 100,
-                                                                                                  avg_test_dice * 100))
+            logging.info('Iter {}-{} eval: \t Loss: {:.6f}\t acc: {:.6f}%\t dice: {:.6f}%'.format(
+                                                                                                    iteration - args.log_interval,
+                                                                                                    iteration,
+                                                                                                    avg_test_loss,
+                                                                                                    epoch_test_accuracy,
+                                                                                                    avg_test_dice))
 
-        if avg_test_dice > best_test_dice:
-            best_test_dice = avg_test_dice
-            logging.info('--- Saving model at Avg Train Dice:{:.2f}%  ---'.format(avg_test_dice * 100))
-            torch.save(model.state_dict(), os.path.join(args.weight, './IterativeFCN_best_valid.pth'))
+            if avg_test_dice > best_test_dice:
+                best_test_dice = avg_test_dice
+                logging.info('--- Saving model at Avg Train Dice:{:.2f}%  ---'.format(avg_test_dice))
+                torch.save(model.state_dict(), os.path.join(args.weight, './IterativeFCN_best_valid.pth'))
 
-        train_loss.append(epoch_train_loss)
-        test_loss.append(epoch_test_loss)
-        train_acc.append(epoch_train_accuracy)
-        test_acc.append(epoch_test_accuracy)
+            train_loss.append(epoch_train_loss)
+            test_loss.append(epoch_test_loss)
+            train_acc.append(epoch_train_accuracy)
+            test_acc.append(epoch_test_accuracy)
 
-        # save snapshot for resume training
-        logging.info('--- Saving snapshot ---')
-        torch.save({
-            'epoch': epoch,
-            'model_state_dict': model.state_dict(),
-            'optimizer_state_dict': optimizer.state_dict(),
-            'train_loss': train_loss,
-            'test_loss': test_loss,
-            'train_acc': train_acc,
-            'test_acc': test_acc,
-            'train_dice': train_dice,
-            'test_dice': test_dice,
-            'best_train_loss': best_train_loss,
-            'best_test_dice': best_test_dice}, os.path.join(args.checkpoints, 'latest_checkpoints'))
+            # save snapshot for resume training
+            logging.info('--- Saving snapshot ---')
+            torch.save({
+                'iteration': iteration,
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'train_loss': train_loss,
+                'test_loss': test_loss,
+                'train_acc': train_acc,
+                'test_acc': test_acc,
+                'train_dice': train_dice,
+                'test_dice': test_dice,
+                'best_train_loss': best_train_loss,
+                'best_test_dice': best_test_dice}, os.path.join(args.checkpoints, 'latest_checkpoints.pth'))
 
-        logging.info("--- %s seconds ---" % (time.time() - start_time))
+            logging.info("--- %s seconds ---" % (time.time() - start_time))
+
+        iteration += 1
